50f65e3b3d806b43f30709f5146df84078f4a591
SpeedPesto <speederyeti@gmail.com>
2026-02-14 23:52:57 +0100

    eee

--- BaseCharacter.gd
+++ BaseCharacter.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 class_name BaseCharacter
 
-@export var tilt_amount = 10.0
+@export var tilt_amount = 5.0
 @export var tilt_speed = 10.0
+@export var bounce_amount = 3.0
+@export var bounce_speed = 10.0
 
 func _physics_process(delta):
 	apply_tilt_effect(delta)
@@ -13,7 +15,10 @@ func apply_tilt_effect(delta):
 			$Sprite2D.flip_h = false
 		elif velocity.x < 0:
 			$Sprite2D.flip_h = true
-			
+		
 		$Sprite2D.rotation_degrees = sin(Time.get_ticks_msec() / 1000.0 * tilt_speed) * tilt_amount
+		$Sprite2D.position.y = -abs(sin(Time.get_ticks_msec() / 1000.0 * bounce_speed)) * bounce_amount
 	else:
+		# Remettre à zéro quand immobile
 		$Sprite2D.rotation_degrees = lerp($Sprite2D.rotation_degrees, 0.0, delta * 5.0)
+		$Sprite2D.position.y = lerp($Sprite2D.position.y, 0.0, delta * 5.0)
